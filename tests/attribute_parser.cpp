90f66d33c88f16971275301389172e26d5e9deba
sadxussein <kvdworking@gmail.com>
2023-06-27 16:56:27 +0300

    need to finish parser ffs

--- tests/attribute_parser.cpp
+++ tests/attribute_parser.cpp
@@ -5,80 +5,136 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
 using namespace std;
 
-class lines {
+/*class lines {
 public:
-    int level;
-    string line, temp_line, buffer;
-    string parent;
-    string tag;
-    
-    vector<string> values;
-    bool isTagOpened;
-
-    void analyze() {
-        
-        stringstream ss(line);
-        while (!ss.eof()) {
-            ss >> temp_line;
-            if (temp_line[0] == '<') {
-                if (temp_line[1] == '/') {
-                    if (temp_line.erase(0, 2) == tag) {
-                        isTagOpened = false;
-                    }
-                }
-                else {
-                    temp_line.erase(0, 1);
-                    tag = temp_line;
-                }                
-            }            
-            else if (temp_line[temp_line.length() - 1] == '>') {
-                temp_line.pop_back();
-                values.push_back(temp_line);
-                isTagOpened = true;
-            }
-            else if (temp_line != "=") {
-                values.push_back(temp_line);
-            }
-        }
-    }
-
-    lines() {
-        parent = "none";
-        line = "";
-        level = 0;
-        isTagOpened = false;
-    }
-};
+	lines() {
+		parent = nullptr;
+		isOpened = true;
+		level = 0;
+	}
+	void set_parent(lines* p) {	// parent setter
+		parent = p;
+	}
+	lines* get_parent() {
+		return parent;
+	}
+	void set_tag(string t) {
+	}
+	string get_tag() {
+		return tag;
+	}
+	void set_level(int l) {
+		level = l;
+	}
+	int get_level() {
+		return level;
+	}
+	void parse_elements(string str) {
+		regex words("\\w+");
+		sregex_token_iterator it(str.begin(), str.end(), words), end;
+		while (it != end) {
+			values.push_back(it->str());
+			++it;
+		}
+		tag = values[0];
+		cout << "stored values: ";
+		for (string v : values) {
+			cout << v << " ";
+		}
+		cout << endl;
+	}
+	void close() {
+		isOpened = false;
+	}
+private:
+	lines* parent;	// parent pointer
+	vector<string> values;
+	string tag;
+	int level;
+	bool isOpened;
+};*/
 
 int main() {
-    int n, q;  
-    string str;
-    cin >> n >> q;
-    getline(cin, str);
+	int n, q;
+	string str;
+	cin >> n >> q;
+	getline(cin, str);	// this line is required to read 1 empty line before reading input, otherwise it 'eats' first line of user input
+	
+	vector<string> lines(n), queries(q);
+	string query;
+	
+/*	vector<string> queries(q);
+	vector<lines> lines_v;
+	lines* parent_line = nullptr;
+	int level = 0;
 
-    vector<lines> lines_array(n);
-    vector<string> queries_array(q);
-    vector<string> element(n);
 
-    for (int i = 0; i < n; i ++) {
-        getline(cin, lines_array[i].line);
-        lines_array[i].analyze();
+	for (int i=0; i<n; i++) {
+		lines line;
+		getline(cin, str);
+		if (i == 0) {
+			line.parse_elements(str);	// parse values from string
+			lines_v.push_back(line);	// add new first line and it is opened by default
+			parent_line = &lines_v.back();
+			cout << parent_line->get_tag() << endl;
+		} else {
+			if (str[1] != '/') {
+				line.parse_elements(str);
+				line.set_parent(parent_line);	// set parent inside lines vector
+				level++;
+				line.set_level(level);		// set nesting level				
+				lines_v.push_back(line);
+//				parent_line = &lines_v.back();			// set pointer to parent
+
+			}
+			else {
+				line.parse_elements(str);
+				for (lines l : lines_v) {
+					if (line.get_tag() == l.get_tag()) {
+						l.close();
+						if (level > 0) {
+							parent_line = l.get_parent();
+						}
+						else {
+							parent_line = nullptr;
+						}
+						level--;
+						break;
+					}
+				}
+			}
+		}
+	}*/
+
+	for (int i=0; i<n; i++) {
+		getline(cin, lines[i]);
+	}
+
+	for (int i=0; i<q; i++) {
+		int pos = 0;
+        getline(cin, query);
+		vector<string> tags;
+		string tag, key;
+		while ((pos = query.find('.')) != string::npos) {
+			tags.push_back(query.substr(0, pos));
+			query.erase(0, pos+1);
+			cout << query << endl;
+		}
+		if (tags.size() != 0) {
+			pos = tags.back().find('~');
+			key = tags.back().substr(pos);
+		}
+		else {
+			tag = 
+		}
+		
+		for (string s : tags) {
+			cout << s << " ";
+		}
+		cout << key << endl;
     }
-
-    for (int i = 0; i < q; i++) {
-        getline(cin, queries_array[i]);
-    }
-
-    for (lines l : lines_array) {
-        cout << l.line << endl;
-    }
-
-    for (string q : queries_array) {
-        cout << q << endl;
-    }
-
-    return 0;
 }
